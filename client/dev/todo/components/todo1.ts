9db403b879001fec6ba73a3be1579e95cbb38d1b
javs626 <perajavier626@gmail.com>
2016-10-15 02:25:23 -0700

    first commit, adding project

---
+++ client/dev/todo/components/todo1.ts
@@ -0,0 +1,12 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'todo1',
+  templateUrl: 'todo/templates/todo1.html',
+  styleUrls: ['todo/styles/todo.css']
+})
+export class todo1 {
+
+}

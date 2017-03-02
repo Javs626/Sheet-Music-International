191f2b8b65b07630e61a6d4efa2cfd1349ec9a9c
javs626 <perajavier626@gmail.com>
2017-03-01 19:45:54 -0700

    added metadata logic

--- index.js
+++ index.js
@@ -13,7 +13,131 @@ var conn = mongoose.connection;
 
 var sheetMusicFile;
 var gfs;
-
+const pieceType = [
+  "Duets",
+  "piano-concertos",
+  "violin-concertos",
+  "oboe-concertos",
+  "concertos",
+  "overtures",
+  "symphony",
+  "books",
+  "choral",
+  "contata",
+  "Ballata",
+  "Estampie",
+  "Organum",
+  "Saltarello",
+  "Ballade",
+  "Canzona",
+  "Carol",
+  "Chanson",
+  "Fantasia",
+  "Galliard",
+  "Intermedio",
+  "Laude",
+  "Litany",
+  "Madrigal",
+  "Mass",
+  "Opera",
+  "Operetta",
+  "Singspiel",
+  "Zarzuela",
+  "Oratorio",
+  "Polonaise",
+  "Prelude",
+  "Quartet",
+  "Quintet",
+  "Requiem",
+  "Rhapsody",
+  "Rondo",
+  "Scherzo",
+  "Serenade",
+  "Sonata",
+  "Suite",
+  "Waltz",
+  "Ballet",
+  "Blues",
+  "Burlesque",
+  "Cabaret",
+  "Jazz",
+  "jazz standard",
+  "Vaudeville",
+  "Motet",
+  "Pavane",
+  "Ricercar",
+  "Sequence",
+  "Tiento",
+  "Toccata",
+  "Baroque",
+  "Allemande",
+  "Canon",
+  "Cantata",
+  "Chaconne",
+  "Courante",
+  "Fugue",
+  "Gavotte",
+  "Gigue",
+  "Minuet",
+  "Oratorio",
+  "Partita",
+  "Passacaglia",
+  "Passepied",
+  "Sarabande",
+  "Sinfonia",
+  "Bagatelle",
+  "Caprice",
+  "Divertimento",
+  "etude",
+  "Impromptu",
+  "Intermezzo",
+  "Mazurka",
+  "March",
+  "Nocturne"
+];
+const instrumentType = [
+  "Violin",
+  "Viola",
+  "Cello",
+  "Bass",
+  "Flute",
+  "Piccolo",
+  "Oboe",
+  "English horn",
+  "Clarinet",
+  "Bass clarinet",
+  "Bassoon",
+  "Contrabassoon",
+  "Saxophones",
+  "Trumpet",
+  "Horn",
+  "french horn",
+  "Trombone",
+  "Tuba",
+  "Celesta",
+  "Piano",
+  "Harpsichord",
+  "Organ",
+  "Synthesizer",
+  "Harp",
+  "Snare drum",
+  "Bass drum",
+  "Cymbals",
+  "Tambourine",
+  "Triangle",
+  "Xylophone",
+  "Glockenspiel",
+  "Chimes",
+  "Marimba",
+  "Vibraphone",
+  "Soprano",
+  "Mezzo-soprano",
+  "Alto",
+  "Countertenor",
+  "Tenor",
+  "Baritone",
+  "Bass"
+];
 var Grid = require("gridfs-stream");
 Grid.mongo = mongoose.mongo;
 
@@ -91,8 +215,16 @@ conn.once("open", function () {
         var name = fileStats.name;
         var path = root + "\\"; // path without file name
         var fPath = path + name; // path with file name
-        //console.log(fPath);
-      
+        //console.log(name);
+      console.log(fPath);
+      // We need to check if the file is a pdf, it is not a pdf, skip
+      var fileType = name.split('.');
+      fileType = fileType[1].toLowerCase();
+      console.log(fileType[1].toLowerCase());
+
+      if (fileType == "pdf"){
+        
+        console.log("file type pdf confirmed");
 
   // make sure we get a newly initialized levels variable each time (we might not need this)
   var levels = '';
@@ -101,7 +233,7 @@ conn.once("open", function () {
   .split('.').toString()
   .split("/").toString()
   .split("\\").toString()
-  .split('+').toString()
+  .split('+').toString()// we can leverage these characters in search
   .split('~').toString()
   .split(',');
   // filter out any blank entries as a result of string splitting
@@ -111,20 +243,46 @@ conn.once("open", function () {
   //else set it to an empty string
   var pathRootLevel = ((levels[0] != undefined) ? levels[0]:'');
   var composerLevel = ((levels[1] != undefined) ? levels[1]:'');
-  var pieceTypeorTitleLevel = ((levels[2] != undefined) ? levels[2]:'');
-  var pieceTitleLevel = ((levels[3] != undefined) ? levels[3]:'');
+  // depending on the depth of the path, level 2 could either be a type or title
+  var pieceTypeLevel = '';
+  var pieceTitleLevel = '';
+  var pieceInstrumentLevel = '';
+  //var pieceTitleLevel = ((levels[3] != undefined) ? levels[3]:'');
+  if(levels[2] != undefined && getMetadataType(levels[2]) == "pieceTypeLevel"){
+    pieceTypeLevel = levels[2];
+  }
+  else if (levels[2] != undefined && getMetadataType(levels[2]) == "pieceInstrumentLevel"){
+    pieceInstrumentLevel = levels[2];
+  }
+  else if(levels[2] != undefined){
+    pieceTitleLevel = levels[2];
+  }
+
+  if (levels[3] != undefined && getMetadataType(levels[3]) == "pieceInstrumentLevel") {
+    pieceInstrumentLevel = levels[3];
+  }
+  else if(levels[3] != undefined) {
+    pieceTitleLevel = levels[3];
+  }
   //for debugging to see if each level is formatted correctly
-  console.log(pathRootLevel + " " + composerLevel + " " + pieceTypeorTitleLevel + " " + pieceTitleLevel);
+ /* console.log(
+    "Path Root: " + pathRootLevel + 
+    " Composer: " + composerLevel + 
+    " Piece Type: " + pieceTypeLevel + 
+    " Instrument Type: " + pieceInstrumentLevel +
+    " Title: " + pieceTitleLevel);*/
         
-        upload.single("avatar");
-        var writestream = gfs.createWriteStream({
-          filename: name,
-          metadata: {
+        /*
+                  metadata: {
             composerType: pathRootLevel,
             composer: composerLevel,
             pieceTypeorTitle: pieceTypeorTitleLevel,
             pieceTitle: pieceTitleLevel,
-          }
+            approved: true
+          } */
+        upload.single("avatar");
+        var writestream = gfs.createWriteStream({
+          filename: name
         });
         //
         // //pipe multer's temp file /uploads/filename into the stream we created above. On end deletes the temporary file.
@@ -132,6 +290,7 @@ conn.once("open", function () {
           .on("end", function () { })
           .on("err", function () { console.log(success) })
           .pipe(writestream);
+      }
         next();// go to the next file in the tree
       });
     });
@@ -145,6 +304,18 @@ conn.once("open", function () {
     });
   });
 
+  var getMetadataType = function (data) {
+    for (var i = 0; i < pieceType.length; i++) {
+      if (data.toLowerCase() == pieceType[i].toLowerCase()) {
+        return "pieceType";
+      }
+    }
+    for (var i = 0; i < instrumentType.length; i++) {
+      if (data.toLowerCase() == instrumentType[i].toLowerCase()) {
+        return "pieceInstrumentLevel";
+      }
+    }
+  }
 
   app.get("/file/:id", function (req, res) {
     //getFileById(req.params.id);
@@ -201,4 +372,6 @@ app.set("views", "./views");
 if (!module.parent) {
   app.listen(3000);
 }
+
+
 

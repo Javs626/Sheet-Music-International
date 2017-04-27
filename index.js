3c854d29b21c9315829eefef722b1104b480549b
javs626 <perajavier626@gmail.com>
2017-04-27 11:49:41 -0700

    more fixes with file population

--- index.js
+++ index.js
@@ -383,7 +383,7 @@ function removeDuplicates(num) {
       ;
       var uploadCount = 0;
       var sheetMusicPath = './sheet-music/master-composers';
-    walker = walk.walk(sheetMusicPath);
+    walker = walk.walk('sheet-music/master-composers');
 
     walker.on("file", function (root, fileStats, next) {
       fs.readFile(fileStats.name, function () {

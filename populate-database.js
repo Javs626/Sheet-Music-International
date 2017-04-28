af29e2f598bc63a32b44c27aba9181fe8caf8330
Kariniemi <wkariniemi@gmail.com>
2017-04-28 19:22:45 -0400

    Use airbnb eslinter on populate-database.js

--- populate-database.js
+++ populate-database.js
@@ -1,61 +1,47 @@
-"use strict";
-var walk = require('walk'),
-    fs = require('fs'),
-    walker,
-    options;
-//var Grid = require("gridfs-stream");
-//var mongoose = require('mongoose')
+let walk = require('walk'),
+  fs = require('fs'),
+  walker,
+  options;
 
-var MongoClient = require('mongodb').MongoClient
- , assert = require('assert');
+let MongoClient = require('mongodb').MongoClient,
+  assert = require('assert');
 
-var url = 'mongodb://127.0.0.1/test';
+const url = 'mongodb://127.0.0.1/test';
+const instrType = require('./modules/instrumentTypes.js');
+const compType = require('./modules/compositionTypes.js');
 
+const getCompositionTitle = function (composition) {
+  for (let i = composition.length - 1; i > 1; i--) {
+    if (instrType.getInstrument(composition[i]) == '' && compType.getComposition(composition[i]) == '') {
+      return composition[i];
+    }
+  }
+  return '';
+};
 
-var instrType = require('./modules/instrumentTypes.js');
-var compType = require('./modules/compositionTypes.js');
-
-/*
-var uri = 'mongodb://127.0.0.1/'
-var options = { server: { socketOptions: { connectTimeoutMS: 10000 } } }
-mongoose.connect(uri, options)
-var conn = mongoose.connection
-Grid.mongo = mongoose.mongo;
-let gfs = Grid(conn.db);
-*/
-
-let getCompositionTitle = function (composition) {
-	for (var i = composition.length - 1; i > 1; i--) {
-		if (instrType.getInstrument(composition[i]) == '' && compType.getComposition(composition[i]) == '') {
-			return composition[i];
-		}
-	}
-	return '';
-}
-
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, (err, db) => {
   assert.equal(null, err);
-  console.log("Connected correctly to server");
+  console.log('Connected correctly to server');
 
-var uploadCount = 0;
-var sheetMusicPath = '/Sheet-Music/master-composers';
-walker = walk.walk(sheetMusicPath);
+  let uploadCount = 0;
+  const sheetMusicPath = '/Sheet-Music/master-composers';
+  walker = walk.walk(sheetMusicPath);
 
-walker.on("file", function (root, fileStats, next) {
-		fs.readFile(fileStats.name, function () {
-				var name = fileStats.name;
-				var path = root + '\/'; // path without file name
-				var fPath = path + name; // path with file name
-				var shmType = "";
+  walker.on('file', (root, fileStats, next) => {
+    fs.readFile(fileStats.name, () => {
+      const name = fileStats.name;
+      const path = `${root}/`; // path without file name
+      const fPath = path + name; // path with file name
+      let shmType = '';
 
-				uploadCount++;
-				var fileType = name.split('.');
-				fileType = fileType[1].toLowerCase();
+      uploadCount++;
+      let fileType = name.split('.');
+      fileType = fileType[1].toLowerCase();
 
 				// make sure we get a newly initialized levels variable each time (we might not need this)
-				var levels = '';
+      let levels = '';
 				// splits the path string by the characters specified below and then returns levels as an array of strings
-				levels = path
+      levels = path
 					.split('.').toString()
 					.split('/').toString()
 					.split('\\').toString()
@@ -63,55 +49,54 @@ walker.on("file", function (root, fileStats, next) {
 					.split('~').toString()
 					.split(',');
 
-				var metadataTags = levels.toString() + name;
-				metadataTags = metadataTags.split(',');
-				metadataTags = metadataTags.filter(function (metadataTags) { return metadataTags.trim() != '' });
-				metadataTags = metadataTags.toString();
+      let metadataTags = levels.toString() + name;
+      metadataTags = metadataTags.split(',');
+      metadataTags = metadataTags.filter(metadataTags => metadataTags.trim() != '');
+      metadataTags = metadataTags.toString();
 				// console.log("index path: " + metadataTags.toLocaleLowerCase())
-				levels = levels.filter(function (levels) { return levels.trim() != '' });
+      levels = levels.filter(levels => levels.trim() != '');
 
-				var pathRootLevel = ((levels[1] != undefined) ? levels[0].toLowerCase() : '');
-				var composerLevel = ((levels[2] != undefined) ? levels[1].toLowerCase() : '');
-				var typeOfInstrument = instrType.getInstrument(fPath);
-				var typeOfComposition = compType.getComposition(fPath);
-				var compositionTitle = getCompositionTitle(levels);
+      const pathRootLevel = ((levels[1] != undefined) ? levels[0].toLowerCase() : '');
+      const composerLevel = ((levels[2] != undefined) ? levels[1].toLowerCase() : '');
+      const typeOfInstrument = instrType.getInstrument(fPath);
+      const typeOfComposition = compType.getComposition(fPath);
+      const compositionTitle = getCompositionTitle(levels);
 
-				if(pathRootLevel == "Copyrighted-Music-For-Sale"){
-					shmType = "copyrighted";
-				}
-				else{
-					shmType = "public";
-				}
+      if (pathRootLevel == 'Copyrighted-Music-For-Sale') {
+        shmType = 'copyrighted';
+      }				else {
+        shmType = 'public';
+      }
 
-				var note = {
-filename: name,
+      const note = {
+        filename: name,
 
-metadata: {
-composerType: pathRootLevel,
-composerName: composerLevel,
-compositionType: typeOfComposition,
-compositionTitle: compositionTitle,
-instrumentType: typeOfInstrument,
-tags: fPath,
-ipType: shmType,
-approved: true
-}
-};
+        metadata: {
+          composerType: pathRootLevel,
+          composerName: composerLevel,
+          compositionType: typeOfComposition,
+          compositionTitle,
+          instrumentType: typeOfInstrument,
+          tags: fPath,
+          ipType: shmType,
+          approved: true,
+        },
+      };
 
-db.collection('fs.files').insertOne(note, function(err, r) {
-				console.log(fPath);
-		});
+      db.collection('fs.files').insertOne(note, (err, r) => {
+        console.log(fPath);
+      });
 
-next(); // go to the next file in the tree
-});
-});
+      next(); // go to the next file in the tree
+    });
+  });
 
-walker.on('end', function () {
-		console.log('File upload complete');
-		db.close();
-		});
+  walker.on('end', () => {
+    console.log('File upload complete');
+    db.close();
+  });
 
-walker.on('errors', function (root, nodeStatsArray, next) {
-		next();
-		});
+  walker.on('errors', (root, nodeStatsArray, next) => {
+    next();
+  });
 });
